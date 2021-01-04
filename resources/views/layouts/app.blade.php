14267933cf1ef2eb263bba4f848da56c3f47d5b9
Frank Liang <liangfeng610@gmail.com>
2021-01-04 19:48:21 +1300

    added hobbies list and built create new hobby page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>@yield('page_title', 'MyHobbies')</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -33,7 +33,9 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-
+                        <li><a class="nav-link{{ Request::is('/') ? ' active' : '' }}" href="/">Start</a></li>
+                        <li><a class="nav-link{{ Request::is('info') ? ' active' : '' }}"  href="/info">Info</a></li>
+                        <li><a class="nav-link{{ Request::is('hobby*') ? ' active' : '' }}"  href="/hobby">Hobbies</a></li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
@@ -73,6 +75,34 @@
         </nav>
 
         <main class="py-4">
+            @isset($message_success)
+                <div class="container">
+                    <div class="alert alert-success" role="alert">
+                        {!! $message_success !!}
+                    </div>
+                </div>
+            @endisset
+
+            @isset($message_warning)
+                <div class="container">
+                    <div class="alert alert-success" role="alert">
+                        {!! $message_warning !!}
+                    </div>
+                </div>
+            @endisset
+
+
+            @if($errors->any())
+                <div class="container">
+                    <div class="alert alert-danger" role="alert">
+                        <ul class="mb-0">
+                            @foreach($errors->all() as $error)
+                                <li>{!! $error !!}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                </div>
+            @endif
             @yield('content')
         </main>
     </div>
---
+++ resources/views/starting_page.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('Starting Page') }}</div>
+
+                    <div class="card-body">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor incididunt ut labore et dolore
+                        magna aliqua. Amet venenatis urna cursus eget nunc
+                        scelerisque viverra mauris in. Diam maecenas ultricies
+                        mi eget. Venenatis urna cursus eget nunc. Enim diam
+                        vulputate ut pharetra sit amet aliquam id. Sit amet
+                        massa vitae tortor condimentum. Blandit massa enim nec
+                        dui nunc mattis enim ut. Commodo sed egestas egestas
+                        fringilla phasellus. Feugiat scelerisque varius morbi
+                        enim. Ut tortor pretium viverra suspendisse potenti
+                        nullam. Ullamcorper velit sed ullamcorper morbi
+                        tincidunt ornare massa.
+
+                        Enim tortor at auctor urna nunc id cursus. Aliquet eget
+                        sit amet tellus cras adipiscing. Neque viverra justo nec
+                        ultrices. Etiam non quam lacus suspendisse. Tristique
+                        risus nec feugiat in. Id venenatis a condimentum vitae
+                        sapien. Volutpat maecenas volutpat blandit aliquam etiam
+                        erat velit scelerisque. Ridiculus mus mauris vitae
+                        ultricies leo integer malesuada nunc. Tellus in metus
+                        vulputate eu scelerisque felis imperdiet proin
+                        fermentum. At varius vel pharetra vel turpis nunc. Netus
+                        et malesuada fames ac turpis egestas maecenas pharetra.
+                        Facilisis leo vel fringilla est. Suspendisse in est ante
+                        in.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
