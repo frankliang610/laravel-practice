44ce9a758c03888311ce21384f9b7ad408238911
Frank Liang <liangfeng610@gmail.com>
2021-01-06 22:43:07 +1300

    Prepare frontend for images

--- resources/views/hobby/show.blade.php
+++ resources/views/hobby/show.blade.php
@@ -5,41 +5,46 @@
         <div class="row justify-content-center">
             <div class="col-md-11">
                 <div class="card">
-                    <div class="card-header">{{ __('Hobby Details') }}</div>
+                    <div class="card-header">Hobby Detail</div>
 
                     <div class="card-body">
-                        <b>{{ $hobby->name }}</b>
-                        <p>{{ $hobby->description }}</p>
-                        @if($hobby->tags->count() > 0)
-                            <b>Attached Tags: </b> (Click to detach)
-                            <p>
-                                @foreach($hobby->tags as $tag)
-                                    <a href="/hobby/{{ $hobby->id }}/tag/{{ $tag->id }}/detach"><span
-                                            class="badge badge-{{ $tag->style }}">{{ $tag->name }}</span></a>
-                                @endforeach
-                            </p>
-                        @endif
+                        <div class="row">
+                            <div class="col-md-9">
+                                <b>{{$hobby->name}}</b>
+                                <p>{{$hobby->description}}</p>
+                                @if($hobby->tags->count() > 0)
+                                    <b>Used Tags:</b> (Click to remove)
+                                    <p>
+                                        @foreach($hobby->tags as $tag)
+                                            <a href="/hobby/{{$hobby->id}}/tag/{{$tag->id}}/detach"><span class="badge badge-{{ $tag->style }}">{{ $tag->name }}</span></a>
+                                        @endforeach
+                                    </p>
+                                @endif
 
-                        @if($available_tags->count() > 0)
-                            <b>Available Tags: </b> (Click to attach)
-                            <p>
-                                @foreach($available_tags as $tag)
-                                    <a href="/hobby/{{ $hobby->id }}/tag/{{ $tag->id }}/attach"><span
-                                            class="badge badge-{{ $tag->style }}">{{ $tag->name }}</span></a>
-                                @endforeach
-                            </p>
-                        @endif
+                                @if($available_tags->count() > 0)
+                                    <b>Available Tags:</b> (Click to assign)
+                                    <p>
+                                        @foreach($available_tags as $tag)
+                                            <a href="/hobby/{{$hobby->id}}/tag/{{$tag->id}}/attach"><span class="badge badge-{{ $tag->style }}">{{ $tag->name }}</span></a>
+                                        @endforeach
+                                    </p>
+                                @endif
+                            </div>
+                            <div class="col-md-3">
+                                <a href="/img/400x300.jpg" data-lightbox="400x300.jpg" data-title="{{ $hobby->name }}">
+                                    <img class="img-fluid" src="/img/400x300.jpg" alt="">
+                                </a>
+                                <i class="fa fa-search-plus"></i> Click image to enlarge
+                            </div>
+                        </div>
                     </div>
                 </div>
-{{--                <div class="mt-2">--}}
-{{--                    <a href="{{ URL::previous() }}"--}}
-{{--                       class="btn btn-primary btn-sm">--}}
-{{--                        <i class="fas fa-arrow-circle-up"></i>--}}
-{{--                        Back to Overview--}}
-{{--                    </a>--}}
-{{--                </div>--}}
+                <!--
+                <div class="mt-2">
+                    <a class="btn btn-primary btn-sm" href="{{ URL::previous() }}"><i class="fas fa-arrow-circle-up"></i> Back to Overview</a>
+                </div>
+                -->
             </div>
         </div>
     </div>
 @endsection
-
